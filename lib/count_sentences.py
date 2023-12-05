db2071ab3cb7f86e9aa34ccc22b62890d4e2a001
lee-kibugi <Linda.Kibugi@student.Moringa>
2023-12-05 15:54:17 +0300

    completed assignments

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,28 @@
-#!/usr/bin/env python3
-
+import re
 class MyString:
-  pass
+    def __init__(self, value=''):
+        self._value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        if not isinstance(new_value, str):
+            print("The value must be a string.")
+        else:
+            self._value = new_value
+
+    def is_sentence(self):
+        return self.value.endswith('.')
+
+    def is_question(self):
+        return self.value.endswith('?')
+
+    def is_exclamation(self):
+        return self.value.endswith('!')
+
+    def count_sentences(self):
+        sentences = [s.strip() for s in re.split(r'[.!?]', self.value) if s.strip()]
+        return len(sentences)
